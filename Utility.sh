30fda83e756e8754fd90005820de3f62d79435a0
Reuben <your_email@mail.com>
2018-09-17 11:25:06 +0100

    Installing SAP HANA monitoring agent for GCP slackdriver monitoring

--- Utility.sh
+++ Utility.sh
@@ -1,6 +1,7 @@
 #Version: 1.0.0
 #Essential tools and utilities post install
 
+# 1.Install Chromium and Chromium flashplayer
 sudo apt install -y chromium-browser
 cat <<EOF | sudo tee /etc/apt/sources.list.d/non-free.list
 deb http://www.deb-multimedia.org stretch main non-free
@@ -13,3 +14,28 @@ sudo dpkg -i deb-multimedia-keyring_2016.8.1_all.deb || \
 rm -f deb-multimedia-keyring_2016.8.1_all.deb
 sudo apt update -y
 sudo apt install -y flashplayer-chromium
+
+#Install SAP HANA monitoring agents for slackdriver monitoring in GCP
+
+curl https://storage.googleapis.com/gcm-ext/gcm_install.sh | bash -x
+
+#Additional Configuration required after Install
+# In /etc/default/gcm following has to be setup depending on servers
+# ---
+# config:
+#   timestamps_in_output: no
+#   debug_messages: yes
+#   skip_default_queries: yes
+#   queries_directory: /tmp/tmp.kUbZiUaGHa/queries
+#   spool_directory: /tmp/tmp.kUbZiUaGHa/staging
+#   enabled_instances:
+#     - [INSTANCE_NAME]
+#   instances:
+#     - name: [INSTANCE_NAME]
+#       host: [INSTANCE_IP]
+#       port: [PORT_NUMBER]
+#       user: [DB_USERNAME]
+#       password: [YOUR_PASSWORD]
+#       type: [INSTANCE_TYPE]
+#     - name: [INSTANCE_NAME_2]
+#       host: [INSTANCE_IP_2]
